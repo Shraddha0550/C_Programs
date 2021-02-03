7595414c933d9e11195a53b6e7e7ba7bac4d71f7
Shraddha Barge <63111483+Shraddha0550@users.noreply.github.com>
2021-02-02 23:42:57 -0500

    Pattern_Demo

---
+++ Pattern_Printing/Lower_Left.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int i=0,j=0,No=0;
+    printf("==============================\n\n");
+    printf("*******pattern printing********\n\n\n");
+    printf("Enter Number:\n");
+    scanf("%d",&No);
+
+    for(i=1;i<=No;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("\t*");
+        }
+        printf("\n");
+    }
+
+    printf("===============================\n");
+    getch();
+    return 0;
+}
