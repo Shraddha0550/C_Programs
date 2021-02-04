9f583d2791ad44365e658b8865e9fd8e23b4d168
Shraddha Barge <63111483+Shraddha0550@users.noreply.github.com>
2021-02-03 21:48:48 -0500

    Pattern_A

---
+++ Pattern_Printing/Pattern_A.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int i=0,j=0,No=0;
+    printf("==============================\n\n");
+    printf("*******pattern printing********\n\n\n");
+    printf("Enter Number:\n");
+    scanf("%d",&No);
+
+    for(i = 1 ; i <= No ; i++)
+    {
+
+        for(j=1;j<=No;j++)
+        {
+
+            if(j == 1 && i > 1 || i == 1 && j > 1  && j < No  || i == No/2+1 || j == No && i > 1)
+            {
+                printf(" *");
+
+            }
+
+            else
+            {
+                printf("  ");
+            }
+        }
+
+        printf("\n");
+    }
+
+    printf("===============================\n");
+    getch();
+    return 0;
+}
