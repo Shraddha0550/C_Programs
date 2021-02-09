1d6330a2d342cdf3b3fa0817e29952e015b8a095
Shraddha Barge <63111483+Shraddha0550@users.noreply.github.com>
2021-02-09 14:25:46 -0500

    Pattern_X

---
+++ Pattern_Printing/pattern_X.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int i = 0 , j = 0 , No = 0;
+    printf("==============================\n\n");
+    printf("*******pattern printing********\n\n\n");
+    printf("Enter Number:\n");
+    scanf("%d",&No);
+
+    for(i = 1 ; i <= No ; i++)
+    {
+        for(j = 1 ; j <= No ; j++)
+        {
+            if(i == j || i+j == No+1)
+            {
+                printf(" *");
+            }
+            else
+            {
+                printf("  ");
+            }
+        }
+        printf("\n");
+    }
+    printf("===============================\n");
+    getch();
+    return 0;
+}
