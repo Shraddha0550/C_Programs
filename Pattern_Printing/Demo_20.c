74cb79eac4f2ea64748c81f6138079d8318b090f
Shraddha Barge <63111483+Shraddha0550@users.noreply.github.com>
2021-02-23 11:22:12 -0500

    Pattern

---
+++ Pattern_Printing/Demo_20.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int row = 0, col = 0,i = 0, j = 0;
+
+    printf("Enter two numbers\n");
+    scanf("%d%d",&row,&col);
+
+    for(i = 1 ; i <= row; i++)
+    {
+        for(j= col; j >=i  ; j--)
+        {
+           printf("\t%d",j);
+
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
