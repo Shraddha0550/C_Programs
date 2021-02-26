56b869ede5b50ca55c929f2d0b42f15650e6b269
Shraddha Barge <63111483+Shraddha0550@users.noreply.github.com>
2021-02-26 11:23:33 -0500

    Pattern

---
+++ Pattern_Printing/Demo_31.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main()
+{
+    int i = 0,j = 0,k=0,No = 0;
+
+    printf("Enter a Number:\n");
+    scanf("%d",&No);
+
+    for(i= 1;i <= No ;i++)
+    {
+
+         for(j= 1;j <= No ;j++)
+         {
+             if(i >= j)
+             {
+                 printf(" *");
+             }
+             else
+             {
+                 printf("  ");
+             }
+
+         }
+         printf("\n");
+    }
+    return 0;
+}
