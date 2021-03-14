1c0c72eb06e79d4811c1d716acc7631b7165cd3f
Shraddha Barge <63111483+Shraddha0550@users.noreply.github.com>
2021-03-14 21:54:49 +0530

    Update Demo_08.c

--- Pattern_Printing/Demo_08.c
+++ Pattern_Printing/Demo_08.c
@@ -3,12 +3,12 @@ int main()
 {
     int row = 0, col = 0,i = 0, j = 0;
 
-    printf("Enter two numbers\n");
+    printf("Enter two Numbers\n");
     scanf("%d%d",&row,&col);
 
     for(i = row ; i >= 0 ; i--)
     {
-        for(j= col; j >= 0 ; j--)
+        for(j = col; j >= 0 ; j--)
         {
             printf(" %c",j+65);
         }
