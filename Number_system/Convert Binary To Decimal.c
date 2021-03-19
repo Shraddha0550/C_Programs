2d542702ec9b34f5c2c69afb6f0739498602e395
Shraddha Barge <63111483+Shraddha0550@users.noreply.github.com>
2021-03-19 15:30:57 -0400

    Binary To Decimal

---
+++ Number_system/Convert Binary To Decimal.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int No = 0,rem = 0,cnt = 0,res = 0;
+
+    printf("Enter The Binary Number:");
+    scanf("%d",&No);
+
+    while(No != 0)
+    {
+        rem = No % 10;
+        res += rem *pow(2,cnt);
+        No = No /10;
+        cnt++;
+    }
+
+    printf(" The Conversion Of Binary To Decimal Number:%d",res);
+
+    getchar();
+}
