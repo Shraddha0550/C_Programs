f838bdec52f49e969fb51cee6e844cd6f4e6d979
Shraddha Barge <63111483+Shraddha0550@users.noreply.github.com>
2021-10-05 20:44:37 -0400

    Header File Creation

---
+++ Header_File_Creation/HeaderFile.h
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#define PI 3.14
+
+struct Demo
+{
+    int i;
+    int j;
+};
+
+typedef int INTEGER;
+
+void fun();
---
+++ Header_File_Creation/Program.c
@@ -0,0 +1,19 @@
+#include "HeaderFile.h"
+
+int main()
+{
+    INTEGER i = 11;
+
+    struct Demo obj;
+
+    printf("Value Of PI is: %f\n",PI);
+
+    fun();       // Function Call
+
+    return 0;
+}
+
+void fun()        // Function Defination
+{
+    printf("Inside Fun\n");
+}
